1c7886d252a99d6771dfbbb58cbf0bc52f447c02
Stephen <stephen@shopkeep.com>
2015-06-24 10:55:16 -0400

    cleanup of new module

--- lib/schema_tools/modules/object_properties.rb
+++ lib/schema_tools/modules/object_properties.rb
@@ -7,20 +7,24 @@ module SchemaTools
       ONEOF = 'oneOf'
       PROPERTY_CONTAINERS = [ PROPERTIES, ALLOF, ANYOF, ONEOF ]
 
-      def all_properties(schema_hash, properties_hash = ActiveSupport::HashWithIndifferentAccess.new )
+      def all_properties(schema_hash, properties_hash = ActiveSupport::HashWithIndifferentAccess.new)
         return schema_hash unless PROPERTY_CONTAINERS.any? { |container| schema_hash[container] }
 
         PROPERTY_CONTAINERS.reduce(properties_hash) do |properties, container|
           if schema_hash[container]
-            if schema_hash[container].respond_to?(:key?)
+            if schema_hash[container].kind_of?(Array)
+              properties.merge!(parse_sub_schemas(schema_hash[container]))
+            else
               properties.merge!(schema_hash[container])
-            elsif schema_hash[container].kind_of?(Array)
-              schema_hash[container].each do |sub_schema|
-                properties.merge!(all_properties(sub_schema))
-              end
             end
           end
           properties
+        end
+      end
+
+      def parse_sub_schemas(schema_array)
+        schema_array.reduce(ActiveSupport::HashWithIndifferentAccess.new) do |properties, sub_schema|
+          properties.merge!(all_properties(sub_schema))
         end
       end
     end
